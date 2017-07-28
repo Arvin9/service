bc5215b88e281fa6c08c50437fc92c038973c8de
Arvin9 <594113869@qq.com>
2017-07-29 01:25:51 +0800

    init

---
+++ TodoService/src/main/java/site/nebulas/controller/HelloController.java
@@ -0,0 +1,16 @@
+package site.nebulas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2016/12/17.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
+    public String say(){
+        return "Hello";
+    }
+}
